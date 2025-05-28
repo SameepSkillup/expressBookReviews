647cd864bdb5dac97b30f0d1a7da0594d384b546
SameepSkillup <sameep.skilluponline@gmail.com>
2025-05-28 08:03:05 -0400

    initialpush

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,11 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+ if (req.session && req.session.accessToken) {
+        next(); 
+    } else {
+        res.status(401).json({ message: "Unauthorized access. Please log in." });
+    }
 });
  
 const PORT =5000;
